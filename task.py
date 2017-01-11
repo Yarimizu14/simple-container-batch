2772d941de6c7f3215cc003b1db1be92483d037f
Kensei Yarimizu <kensei@abeja.asia>
2017-01-11 13:56:56 +0000

    post datatime as task

--- task.py
+++ task.py
@@ -2,6 +2,8 @@
 # -*- coding: utf-8 -*-
 
 import requests
+from datetime import datetime
 
-r = requests.get('https://www.hashicorp.com/')
+msg = datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
+r = requests.post('http://ec2-54-199-208-148.ap-northeast-1.compute.amazonaws.com/messages/{}'.format(msg))
 print r.content
